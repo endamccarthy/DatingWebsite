bce7a9d33bf5c85c25ba662eda693a5d486ba65d
Enda McCarthy <enda.mccarthy@outlook.com>
2020-02-27 15:05:06 +0000

    started user authentication

--- config.php
+++ config.php
@@ -1,16 +1,17 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
+
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
- 
-/* Attempt to connect to MySQL database */
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+define('DB_PASSWORD', 'root');
+define('DB_NAME', 'dating_website');
+define('DB_PORT', 8889);
+
+// Attempt to connect to MySQL database 
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
  
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+
 ?>
---
+++ pages/templates/top.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="../style/style.css">
+  </head>
+  <body>
